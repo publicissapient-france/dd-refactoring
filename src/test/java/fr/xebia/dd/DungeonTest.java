24013498d877eedd148aaee2b8e9cc6e2b24cb85
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-05-01 12:54:47 +0200

    add specific scenario name into golden tickets

--- src/test/java/fr/xebia/dd/DungeonTest.java
+++ src/test/java/fr/xebia/dd/DungeonTest.java
@@ -21,14 +21,17 @@ public class DungeonTest {
     public SystemOutRule systemOutRule = new SystemOutRule().enableLog().mute();
 
     @Test
-    @Ignore
-    public void golden_ticket_generator() throws IOException {
+//    @Ignore
+    public void golden_ticket_generator() throws Exception {
         for (int i = 0; i < 1000; i++) {
             Dungeon.setRandom(new Random(i));
-            File testFile = new File("/home/dolounet/dev/test-fixtures/test" + i);
-            testFile.delete();
-            testFile.createNewFile();
-            try (FileWriter testFileWriter = new FileWriter(testFile)) {
+            String scenarioName = "monster-kills-player-input";
+            File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
+            Dungeon.setInputFile(inputFile);
+            File outputFile = new File("/home/dolounet/dev/test-fixtures/test" + scenarioName + i);
+            outputFile.delete();
+            outputFile.createNewFile();
+            try (FileWriter testFileWriter = new FileWriter(outputFile)) {
                 systemOutRule.clearLog();
                 Dungeon.main(new String[0]);
                 testFileWriter.append(systemOutRule.getLog());
@@ -41,9 +44,10 @@ public class DungeonTest {
         for (int i = 0; i < 1000; i++) {
             systemOutRule.clearLog();
             Dungeon.setRandom(new Random(i));
-            File inputFile = new File(Resources.getResource("usecases/monster-kills-player-input.txt").toURI());
+            String scenarioName = "monster-kills-player-input";
+            File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
             Dungeon.setInputFile(inputFile);
-            String testFileContent = new String(Files.readAllBytes(Paths.get("/home/dolounet/dev/test-fixtures/test" + i)));
+            String testFileContent = new String(Files.readAllBytes(Paths.get("/home/dolounet/dev/test-fixtures/test" + scenarioName + i)));
 
             Dungeon.main(new String[0]);
 
