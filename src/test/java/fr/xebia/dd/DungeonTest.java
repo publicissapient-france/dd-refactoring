cb2962e3bdce8214738c248a00ec07e9868b961e
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-05-01 12:34:50 +0200

    test with golden ticket

--- src/test/java/fr/xebia/dd/DungeonTest.java
+++ src/test/java/fr/xebia/dd/DungeonTest.java
@@ -1,5 +1,7 @@
 package fr.xebia.dd;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
@@ -7,7 +9,11 @@ import org.junit.contrib.java.lang.system.SystemOutRule;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DungeonTest {
 
@@ -15,13 +21,14 @@ public class DungeonTest {
     public SystemOutRule systemOutRule = new SystemOutRule().enableLog().mute();
 
     @Test
+    @Ignore
     public void golden_ticket_generator() throws IOException {
         for (int i = 0; i < 1000; i++) {
             Dungeon.setRandom(new Random(i));
             File testFile = new File("/home/dolounet/dev/test-fixtures/test" + i);
             testFile.delete();
             testFile.createNewFile();
-            try (FileWriter testFileWriter = new FileWriter(testFile)){
+            try (FileWriter testFileWriter = new FileWriter(testFile)) {
                 systemOutRule.clearLog();
                 Dungeon.main(new String[0]);
                 testFileWriter.append(systemOutRule.getLog());
@@ -29,4 +36,16 @@ public class DungeonTest {
         }
     }
 
+    @Test
+    public void should_be_consistent_with_golden_ticket() throws Exception {
+        for (int i = 0; i < 1000; i++) {
+            systemOutRule.clearLog();
+            Dungeon.setRandom(new Random(i));
+            String testFileContent = new String(Files.readAllBytes(Paths.get("/home/dolounet/dev/test-fixtures/test" + i)));
+
+            Dungeon.main(new String[0]);
+
+            assertThat(systemOutRule.getLog()).isEqualTo(testFileContent);
+        }
+    }
 }
