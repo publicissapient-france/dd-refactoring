a3d9e917fa25f5254e83a51ba374538e38ebefa1
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-05-01 12:59:32 +0200

    big fucking golden ticket

--- src/test/java/fr/xebia/dd/DungeonTest.java
+++ src/test/java/fr/xebia/dd/DungeonTest.java
@@ -9,6 +9,7 @@ import org.junit.contrib.java.lang.system.SystemOutRule;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -21,37 +22,47 @@ public class DungeonTest {
     public SystemOutRule systemOutRule = new SystemOutRule().enableLog().mute();
 
     @Test
-//    @Ignore
+    @Ignore
     public void golden_ticket_generator() throws Exception {
         for (int i = 0; i < 1000; i++) {
             Dungeon.setRandom(new Random(i));
-            String scenarioName = "monster-kills-player-input";
-            File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
-            Dungeon.setInputFile(inputFile);
-            File outputFile = new File("/home/dolounet/dev/test-fixtures/test" + scenarioName + i);
-            outputFile.delete();
-            outputFile.createNewFile();
-            try (FileWriter testFileWriter = new FileWriter(outputFile)) {
-                systemOutRule.clearLog();
-                Dungeon.main(new String[0]);
-                testFileWriter.append(systemOutRule.getLog());
-            }
+            writeScenarioIteration(i, "monster-kills-player-input");
+            writeScenarioIteration(i, "player-escapes-without-fighting-input");
+            writeScenarioIteration(i, "player-kills-monster-input");
+        }
+    }
+
+    private void writeScenarioIteration(int i, String scenarioName) throws URISyntaxException, IOException {
+        File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
+        Dungeon.setInputFile(inputFile);
+        File outputFile = new File("/home/dolounet/dev/test-fixtures/test" + scenarioName + i);
+        outputFile.delete();
+        outputFile.createNewFile();
+        try (FileWriter testFileWriter = new FileWriter(outputFile)) {
+            systemOutRule.clearLog();
+            Dungeon.main(new String[0]);
+            testFileWriter.append(systemOutRule.getLog());
         }
     }
 
     @Test
     public void should_be_consistent_with_golden_ticket() throws Exception {
         for (int i = 0; i < 1000; i++) {
-            systemOutRule.clearLog();
-            Dungeon.setRandom(new Random(i));
-            String scenarioName = "monster-kills-player-input";
-            File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
-            Dungeon.setInputFile(inputFile);
-            String testFileContent = new String(Files.readAllBytes(Paths.get("/home/dolounet/dev/test-fixtures/test" + scenarioName + i)));
-
-            Dungeon.main(new String[0]);
-
-            assertThat(systemOutRule.getLog()).isEqualTo(testFileContent);
+            assertScenarioIteration(i, "monster-kills-player-input");
+            assertScenarioIteration(i, "player-escapes-without-fighting-input");
+            assertScenarioIteration(i, "player-kills-monster-input");
         }
+    }
+
+    private void assertScenarioIteration(int i, String scenarioName) throws URISyntaxException, IOException {
+        systemOutRule.clearLog();
+        Dungeon.setRandom(new Random(i));
+        File inputFile = new File(Resources.getResource("usecases/" + scenarioName + ".txt").toURI());
+        Dungeon.setInputFile(inputFile);
+        String testFileContent = new String(Files.readAllBytes(Paths.get("/home/dolounet/dev/test-fixtures/test" + scenarioName + i)));
+
+        Dungeon.main(new String[0]);
+
+        assertThat(systemOutRule.getLog()).isEqualTo(testFileContent);
     }
 }
